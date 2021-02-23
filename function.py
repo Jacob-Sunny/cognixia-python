b7bcb174c56ab8cc838f451f17f0f5a09b5d1a3d
Jacob-Sunny <jacobsunny12@yahoo.com>
2021-02-23 17:53:02 -0500

    Function basics

---
+++ function.py
@@ -0,0 +1,16 @@
+def weird_arithmetic(x,y,z): #function definition
+    print((x**x + y**z)//z) #code block   
+weird_arithmetic(5,6,7) #function call
+
+def myFunction(greeting,name):
+    print(greeting + " " + name)
+myFunction("Hello","Jacob")
+
+def returnFunction(x):
+    return x
+someNumber = returnFunction(10)
+print(someNumber)
+
+
+
+
