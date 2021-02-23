d347b2b1bb3e682c34c2c1c4e8f14d89e7a8a940
Jacob-Sunny <jacobsunny12@yahoo.com>
2021-02-23 17:13:31 -0500

    Basic math computations

--- introduction.py
+++ introduction.py
@@ -4,3 +4,19 @@ print("Hello", name + "!")
 name = "Tim"
 
 print("Name was changed to : " + name)
+
+print(1+6)  # addition
+print(5 * 5) # multiplication
+print(7/2) # float division
+print(7//2) # whole number dvivision
+print(2**4) # power
+print(27 % 3) # modual
+print("~~~~~~~~~~~~~~~")
+var1 = 10
+var2 = 20
+
+print(var1 * var2)
+print(var1/var2)
+print(var1//var2)
+print(var1**var2)
+print(var1%var2)
