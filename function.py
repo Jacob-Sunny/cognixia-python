679679e2fbb1205fbf36dd4e0d701e9d047b6b2a
Jacob-Sunny <jacobsunny12@yahoo.com>
2021-02-24 10:55:23 -0500

    Circle Functions and Volume Function

--- function.py
+++ function.py
@@ -10,8 +10,22 @@ def returnFunction(x):
     return x
 someNumber = returnFunction(10)
 print(someNumber)
-
-
+print("**********Area Of Circle**************")
+#AREA OF CIRCLE
+def areaFunction(radius):
+    PI = 3.14
+    return PI*(radius**2)
+r = 4
+AreaOFCircle = areaFunction(4)
+print(AreaOFCircle)
+print("***********Volume Of Cylinder******************")
+#vOLUME OF CYLINDER
+def volumeFunction(radius,height):
+    PI = 3.14
+    return PI*radius**2*height
+h = 10
+VolumeOFCyl = volumeFunction(r,h)
+print(VolumeOFCyl)    
 
 
 
